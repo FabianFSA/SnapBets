60dba5559268e3112a720de09cea091ed3b957e0
Fabian <hqfub@gmail.com>
2025-11-14 20:54:36 +0100

    - Hello World with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import logIn from "./components/login.tsx";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ src/components/login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function logIn() {
+  return <p>"Hello Hi"</p>;
+}
+
+export default logIn;

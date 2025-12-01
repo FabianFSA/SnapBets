580e37030226d84b16b921e04215a8c1ce3bf052
Fabian <hqfub@gmail.com>
2025-12-01 20:10:24 +0100

    - add mock datas

---
+++ src/mocks/games.ts
@@ -0,0 +1,182 @@
+export const Games = [
+  {
+    id: "G1",
+    homeTeam: "Arizona Cardinals", // Cardinals
+    guestTeam: "Los Angeles Rams", // Rams
+    season: 2024,
+    week: 1,
+    startingTime: "2024-09-08T18:00:00Z",
+    score: { home: 20, guest: 27 }, // Rams win
+  },
+  {
+    id: "G2",
+    homeTeam: "Kansas City Chiefs", // Chiefs
+    guestTeam: "Denver Broncos", // Broncos
+    season: 2024,
+    week: 1,
+    startingTime: "2024-09-08T21:25:00Z",
+    score: { home: 31, guest: 17 }, // Chiefs win
+  },
+  {
+    id: "G3",
+    homeTeam: "Green Bay Packers", // Packers
+    guestTeam: "Chicago Bears", // Bears
+    season: 2024,
+    week: 1,
+    startingTime: "2024-09-08T18:00:00Z",
+    score: { home: 23, guest: 20 }, // Packers win
+  },
+  {
+    id: "G4",
+    homeTeam: "Buffalo Bills", // Bills
+    guestTeam: "Miami Dolphins", // Dolphins
+    season: 2024,
+    week: 1,
+    startingTime: "2024-09-08T21:25:00Z",
+    score: { home: 28, guest: 28 }, // Tie
+  },
+  {
+    id: "G5",
+    homeTeam: "Los Angeles Rams",
+    guestTeam: "Kansas City Chiefs",
+    season: 2024,
+    week: 2,
+    startingTime: "2024-09-15T18:00:00Z",
+    score: { home: 17, guest: 30 }, // Chiefs win
+  },
+  {
+    id: "G6",
+    homeTeam: "Denver Broncos",
+    guestTeam: "Arizona Cardinals",
+    season: 2024,
+    week: 2,
+    startingTime: "2024-09-15T21:25:00Z",
+    score: { home: 13, guest: 10 }, // Broncos win
+  },
+  {
+    id: "G7",
+    homeTeam: "Chicago Bears",
+    guestTeam: "Buffalo Bills",
+    season: 2024,
+    week: 2,
+    startingTime: "2024-09-15T18:00:00Z",
+    score: { home: 10, guest: 24 }, // Bills win
+  },
+  {
+    id: "G8",
+    homeTeam: "Miami Dolphins",
+    guestTeam: "Green Bay Packers",
+    season: 2024,
+    week: 2,
+    startingTime: "2024-09-15T21:25:00Z",
+    score: { home: 14, guest: 27 }, // Packers win
+  },
+  {
+    id: "G9",
+    homeTeam: "Kansas City Chiefs",
+    guestTeam: "Arizona Cardinals",
+    season: 2024,
+    week: 3,
+    startingTime: "2024-09-22T18:00:00Z",
+    score: { home: 35, guest: 13 }, // Chiefs win
+  },
+  {
+    id: "G10",
+    homeTeam: "Los Angeles Rams",
+    guestTeam: "Denver Broncos",
+    season: 2024,
+    week: 3,
+    startingTime: "2024-09-22T21:25:00Z",
+    score: { home: 24, guest: 21 }, // Rams win
+  },
+  {
+    id: "G11",
+    homeTeam: "Green Bay Packers",
+    guestTeam: "Buffalo Bills",
+    season: 2024,
+    week: 3,
+    startingTime: "2024-09-22T18:00:00Z",
+    score: { home: 20, guest: 31 }, // Bills win
+  },
+  {
+    id: "G12",
+    homeTeam: "Chicago Bears",
+    guestTeam: "Miami Dolphins",
+    season: 2024,
+    week: 3,
+    startingTime: "2024-09-22T21:25:00Z",
+    score: { home: 17, guest: 34 }, // Dolphins win
+  },
+  {
+    id: "G13",
+    homeTeam: "Arizona Cardinals",
+    guestTeam: "Green Bay Packers",
+    season: 2024,
+    week: 4,
+    startingTime: "2024-09-29T18:00:00Z",
+    score: { home: 16, guest: 28 }, // Packers win
+  },
+  {
+    id: "G14",
+    homeTeam: "Kansas City Chiefs",
+    guestTeam: "Buffalo Bills",
+    season: 2024,
+    week: 4,
+    startingTime: "2024-09-29T21:25:00Z",
+    score: { home: 27, guest: 30 }, // Bills win
+  },
+  {
+    id: "G15",
+    homeTeam: "Denver Broncos",
+    guestTeam: "Miami Dolphins",
+    season: 2024,
+    week: 4,
+    startingTime: "2024-09-29T18:00:00Z",
+    score: { home: 21, guest: 21 }, // Tie
+  },
+  {
+    id: "G16",
+    homeTeam: "Chicago Bears",
+    guestTeam: "Los Angeles Rams",
+    season: 2024,
+    week: 4,
+    startingTime: "2024-09-29T21:25:00Z",
+    score: { home: 13, guest: 19 }, // Rams win
+  },
+  {
+    id: "G17",
+    homeTeam: "Buffalo Bills",
+    guestTeam: "Arizona Cardinals",
+    season: 2024,
+    week: 5,
+    startingTime: "2024-10-06T18:00:00Z",
+    score: { home: 34, guest: 10 }, // Bills win
+  },
+  {
+    id: "G18",
+    homeTeam: "Miami Dolphins",
+    guestTeam: "Kansas City Chiefs",
+    season: 2024,
+    week: 5,
+    startingTime: "2024-10-06T21:25:00Z",
+    score: { home: 17, guest: 38 }, // Chiefs win
+  },
+  {
+    id: "G19",
+    homeTeam: "Green Bay Packers",
+    guestTeam: "Los Angeles Rams",
+    season: 2024,
+    week: 5,
+    startingTime: "2024-10-06T18:00:00Z",
+    score: { home: 24, guest: 28 }, // Rams win
+  },
+  {
+    id: "G20",
+    homeTeam: "Denver Broncos",
+    guestTeam: "Chicago Bears",
+    season: 2024,
+    week: 5,
+    startingTime: "2024-10-06T21:25:00Z",
+    score: { home: 20, guest: 23 }, // Bears win
+  },
+];

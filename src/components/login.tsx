35a6e9f03c8ec348e1c56aa6a66d0e735a77e82b
Fabian <hqfub@gmail.com>
2026-02-05 08:02:53 +0100

    add: components and build the login page a bit more

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,6 +3,14 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { useNavigate } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Field, FieldGroup, FieldLabel } from "./ui/field";
 function LogInC() {
   const [value, setValue] = useState("");
   const navigate = useNavigate();
@@ -12,22 +20,45 @@ function LogInC() {
   }
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <Input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        type="email"
-        placeholder="Email"
-        required
-      />
-      <Input type="password" placeholder="Password" />
-      <Button type="submit" variant="default">
-        Log in!
-      </Button>
-      <Label>Das ist deine E-Mail! {value}</Label>
-      <Button type="button" onClick={handleReturn}>
-        Return to Homepage
-      </Button>
+    <div className="bg-muted flex min-h-svh w-full items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login zu deinem Account</CardTitle>
+              <CardDescription>
+                Gebe hier deine E-Mail und dein Passwort ein um dich einzuloggen
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <CardContent>
+            <CardContent>
+              <form>
+                <FieldGroup>
+                  <Field>
+                    <FieldLabel htmlFor="email">E-Mail</FieldLabel>
+                    <Input
+                      value={value}
+                      onChange={(e) => setValue(e.target.value)}
+                      type="email"
+                      placeholder="Email"
+                      required
+                    />
+                  </Field>
+                </FieldGroup>
+              </form>
+            </CardContent>
+          </CardContent>
+        </div>
+        <Input type="password" placeholder="Password" />
+        <Button type="submit" variant="default">
+          Log in!
+        </Button>
+        <Label>Das ist deine E-Mail! {value}</Label>
+        <Button type="button" onClick={handleReturn}>
+          Return to Homepage
+        </Button>
+      </div>
     </div>
   );
 }

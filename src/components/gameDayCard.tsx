7a8bffef41922eae3bf0702be883e6040a337bf2
Fabian <hqfub@gmail.com>
2026-04-25 17:36:04 +0200

    add: checkboxes change: GameDayCards

--- src/components/gameDayCard.tsx
+++ src/components/gameDayCard.tsx
@@ -1,5 +1,6 @@
 import { Game } from "../../Entities/Game.ts";
-import { Card, CardContent } from "./ui/card";
+import { Card, CardContent, CardTitle } from "./ui/card";
+import { Checkbox } from "./ui/checkbox.tsx";
 
 interface GameCardProps {
   game: Game;
@@ -8,36 +9,41 @@ interface GameCardProps {
 export function GameCard({ game }: GameCardProps) {
   return (
     <Card>
-      <CardContent className="p-4">
-        <div>
-          {/* Game Week */}
-          <div>
-            <span> Week {game.week}</span>
-          </div>
+      <CardTitle>
+        {game.homeTeam.name} @ {game.awayTeam.name}
+      </CardTitle>
+      <CardContent className="p-3">
+        <div className="flex flex-row items-center p-3 font-bold">
           {/* // Home Team */}
-          <div>
-            <img
-              src={game.homeTeam.logo}
-              alt={game.homeTeam.name}
-              className="w-20 h-20 object-contain"
-            />
-            <span> {game.homeTeam.name}</span>
-          </div>
-          {/* Score */}
-          <div>
+          <div className="basis-1/6"> {game.homeTeam.name}</div>
+
+          <img
+            src={game.homeTeam.logo}
+            alt={game.homeTeam.name}
+            className="w-20 h-20 object-contain basis-l"
+          />
+          <Checkbox id="homeTeam" name="homeTeamCheckBox"></Checkbox>
+
+          {/***
+           * Score
+           * TODO
+           * Sollte erst eingeblendet werden wenn der Spieltag schon vorbei ist
+           */}
+          {/* <div className="font-extrabold">
             <span>{game.homeScore || 0}</span>
             <span> : </span>
             <span>{game.awayScore || 0}</span>
-          </div>
+          </div> */}
           {/* Away Team */}
-          <div>
+          <Checkbox></Checkbox>
+          <div className="basis-1/6">
             <img
               src={game.awayTeam.logo}
               alt={game.awayTeam.name}
-              className="w-20 h-20 mb-2 object-contain"
+              className="w-20 h-20 object-contain basis-l"
             />
-            <span> {game.awayTeam.name}</span>
           </div>
+          <div className="basis-1/6"> {game.awayTeam.name}</div>
         </div>
       </CardContent>
     </Card>
--- src/components/teamGameDayCards.tsx
+++ src/components/teamGameDayCards.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function TeamGameDayCards({ Games }: Props) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+    <div className=" grid grid-cols-1 p-3 bg-blue-400">
       {Games.map((game) => (
         <GameCard key={game.id} game={game} />
       ))}
